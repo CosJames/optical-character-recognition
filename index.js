5b0922304b790b6e58d7170809688f8a1d9cdeec
James Daniel Cos <codanielyt@gmail.com>
2023-04-17 03:17:30 +0800

    Added some test algorithm

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const imagePath = fs.readFileSync(__dirname + "/assets/example.png");
 Jimp.read(imagePath)
   .then((image) => {
     image
-      .resize(950, Jimp.AUTO) // resize
-      .quality(100) // set JPEG quality
+      .resize(900, Jimp.AUTO) // resize
+      .quality(60) // set JPEG quality
       .greyscale() // set greyscale
       .write("jimpified.jpg"); // save
 
@@ -16,35 +16,40 @@ Jimp.read(imagePath)
     const tempImagePath = "jimpified.jpg";
     return image.writeAsync(tempImagePath);
   })
-  .then((r) => {
+  .then(async (r) => {
     const imagePath = fs.readFileSync(__dirname + "/jimpified.jpg");
 
     const config = {
       lang: "eng",
-      oem: 3,
-      psm: 3,
+      tessedit_char_whitelist: '0123456789PRN', // Specify the characters to whitelist for recognition
+      //tessedit_ocr_engine_mode: 'OEM_TESSERACT_ONLY', // Set the OCR engine mode (if applicable)
+      image: {
+        buffer: imagePath,
+      },
     };
 
-    Tesseract.recognize(
+    const result = await Tesseract.recognize(
       imagePath,
       config
     )
-      .then((text) => {
-        console.log(text);
-        // console.log("Result:", text);
-        // console.log('Extracted Text:', text);
 
-        // Extract the PNR using pattern matching
-        const regex = /PRN\s+(\d+)/; // Assumes PRN is followed by whitespace and then a sequence of digits
-        const prnMatch = text.match(regex);
-        const prn = prnMatch ? prnMatch[0] : null;
-        console.log("Extracted PRN:", prn);
-      })
-      .catch((error) => {
-        console.log("ERROR");
+    console.log(result)
+      // .then((text) => {
+      //   console.log('OCR Result:', text);
+      //   // Clean up recognized text by removing non-numeric characters
+      //   const cleanedText = text.replace(/\D+/g, ''); // Clean up recognized text
+      //   const pattern = /^PRN\s\d{9}$/; // Define expected pattern
+      //   if (pattern.test(cleanedText)) {
+      //     console.log('Image contains a postal ID');
+      //   } else {
+      //     console.log('Image does not contain a postal ID');
+      //   }
+      // })
+      // .catch((error) => {
+      //   console.log("ERROR");
 
-        console.log(error.message);
-      });
+      //   console.log(error.message);
+      // });
   })
   .catch((error) => {
     console.error("Image Processing Error:", error);
